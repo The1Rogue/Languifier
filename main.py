2fcff336fd6a1489e7f6132d29052b6fa3a74502
the1Rogue <fabiantschut@gmail.com>
2021-10-20 14:53:53 +0200

    added textwrapping and read from image

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import tkinter as tk
 import tkinter.font as font
+import pytesseract as pt
+from PIL import Image
 
 wn = tk.Tk()
 wn.attributes("-fullscreen", True)
 
-screenWidth, screenHeight = wn.winfo_screenwidth(), wn.winfo_screenheight()
+screenWidth, screenHeight = 1920, 1080 #wn.winfo_screenwidth(), wn.winfo_screenheight()
+print(screenWidth)
 
-helv = font.Font(family = "Helvetica", size = 20)
+monoFont = font.Font(family ="NotoSansMono Nerd Font", size = 20)
 
-file = open("testtext.txt")
-string = file.read()
-file.close()
 
 def wrapper(s):
     return lambda: print(s)
@@ -21,36 +21,55 @@ def createButtonFrame(string):
 
     out = tk.Frame(wn)
 
-    line = tk.Frame(out)
     lineIndex = 0
+    line = tk.Frame(out)
     column = 0
+
+    width = 0
 
     for i in range(len(data)):
         if data[i].startswith("\n"):
-            line.grid(column=0, row=lineIndex, sticky="nw")
-            lineIndex += 1
-            line = tk.Frame(out)
-            column = 0
             data[i] = data[i][1:]
+            width = screenWidth
 
         if data[i] == "":
             continue
 
+
+        w = len(data[i])*16 + 28
+
+        if width + w > screenWidth:
+            print(data[i])
+            line.grid(column=0, row=lineIndex, sticky="nw")
+            lineIndex += 1
+            line = tk.Frame(out)
+            column = 0
+            width = 0
+
         f = wrapper(data[i])
-        tk.Button(line, text = data[i], font = helv, command = f).grid(column = column, row = 0)
+        button = tk.Button(line, text=data[i], font=monoFont, command=f)
+
+
+
+        button.grid(column=column, row=0)
+        width += w
         column += 1
 
     line.grid(column=0, row=lineIndex, sticky="nw")
     return out
 
+def readFromImage(img):
+    return pt.image_to_string(img)
 
 topBar = tk.Frame(wn, height = 64, width = screenWidth)
 
-settings = tk.Button(topBar, text = "settings", font = helv)
+settings = tk.Button(topBar, text = "settings", font = monoFont)
 settings.pack(side = "right")
 
 topBar.pack()
 topBar.pack_propagate(0)
+
+string = readFromImage(Image.open("testImg.png"))
 
 ButtonFrame = createButtonFrame(string)
 ButtonFrame.pack(side = "top")
