410aad342948d9897941ffe56795f468586723cf
the1Rogue <fabiantschut@gmail.com>
2021-10-28 21:12:56 +0200

    inverts image, so now it reads black on white text as white on black

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import tkinter.font as font
 import pytesseract as pt
 from PIL import Image, ImageTk
 import screen_brightness_control as pct
-# import picamera as pc
-# from gpiozero import Button as btn
+import picamera as pc
+from gpiozero import Button as btn
 import requests
 from bs4 import BeautifulSoup as bs
+import wifi
 
 wn = tk.Tk()
 wn.attributes("-fullscreen", True)
@@ -19,7 +20,7 @@ screenWidth, screenHeight = wn.winfo_screenwidth(), wn.winfo_screenheight()
 
 monoFont = font.Font(family ="NotoSansMono Nerd Font", size=20)
 monoFont1 = font.Font(family ="NotoSansMono Nerd Font", size=20, weight='bold')
-#bttn = btn(21)
+bttn = btn(21)
 
 def wrapper(word, parent):
     return lambda: setMainFrame(getWordFrame(word, parent))
@@ -47,7 +48,7 @@ def release(event):
     mouseData[0] = False
 
 def createButtonFrame():
-    string = readFromImage(Image.open("tmp.png"))
+    string = readFromImage("tmp.rgb")
     string = string.replace("\n", " \n")
     data = string.split(" ")
 
@@ -103,18 +104,25 @@ def createButtonFrame():
 
     return out
 
-def readFromImage(img):
-    return 'This is a test text'
-    # return pt.image_to_string(img)
+def readFromImage(file):
+    with open(file, "rb") as f:
+        raw = f.read()
+    f.close()
 
-# def camera():
-#     global ButtonFrame
-#     with pc.PiCamera() as cam:
-#         cam.start_preview()
-#         bttn.wait_for_press()
-#         cam.stop_preview()
-#         cam.capture("tmp.png")
-#     ButtonFrame = createButtonFrame()
+    raw.translate(bytes(range(255,-1,-1)))
+
+    img = Image.frombytes("RGB", (800, 480), raw)
+
+    return pt.image_to_string(img)
+
+def camera():
+    global ButtonFrame
+    with pc.PiCamera() as cam:
+        cam.start_preview()
+        bttn.wait_for_press()
+        cam.stop_preview()
+        cam.capture("tmp.rgb", format = "rgb")
+    ButtonFrame = createButtonFrame()
 
 def scrape(word):
     response = requests.get('https://www.thesaurus.com/browse/{}'.format(word))
@@ -144,11 +152,42 @@ def scrape(word):
 
     return synonyms
 
+def wifiWrapper(pssw, Cell):
+    def tmp():
+        s = wifi.Scheme.for_cell("wlan0", Cell.ssid, Cell, pssw.get())
+        s.save()
+        s.activate()
+
+    return tmp
+
 def getSettingsFrame():
     out = tk.Frame(wn, bd=1, relief=tk.RAISED, padx=20, pady=20)
 
     tk.Label(out, text="Settings Page", font=('Helvetica', 15, 'bold')).grid(row=0, column=1)
-    tk.Button(out, text="WiFi", font='Helvetica', relief=tk.FLAT).grid(row=1, column=0)
+    tk.Label(out, text="WiFi", font='Helvetica').grid(row=1, column=0)
+
+    wifiFrame = tk.Frame(out)
+    ssids = []
+
+    w = list(wifi.Cell.all("wlan0"))
+    for i in range(len(w)):
+        if w[i].ssid in ssids:
+            continue
+
+        ssids.append(w[i].ssid)
+
+        f = Frame(wifiFrame)
+        tk.Label(f, text=w[i].ssid, font=monoFont).grid(row=0,column=0)
+        pssw = tk.StringVar()
+        tk.Entry(f, textvariable = pssw).grid(row=0,column=1)
+        tk.Button(f, text="connect", font=monoFont, command=wifiWrapper(pssw,w[i])).grid(row=0,column=2)
+
+        if len(ssids) >= 5:
+            break
+    wifiFrame.grid(row=1,column=1)
+
+
+
     tk.Label(out, text="Brightness", font='Helvetica').grid(row=2, column=0, sticky="nw")
     bright = Scale(out, from_=0, to=100, orient=HORIZONTAL, resolution=10, length=200, command=change_bright, )
     bright.grid(row=2, column=1)
@@ -199,19 +238,19 @@ topBar.pack(side = "top", fill = "x")
 tk.Label(topBar, text="Which word would you like to simplify?", font=monoFont1).pack(side='bottom')
 settingsFrame = getSettingsFrame()
 
-photo_set = Image.open('settingsIcon.png')
-photo_set = photo_set.resize((200, 200))
+photo_set = Image.open('root/settingsIcon.png')
+photo_set = photo_set.resize((40, 40))
 img_set = ImageTk.PhotoImage(photo_set)
 settingsButton = tk.Button(topBar, text="settings", image=img_set, font=monoFont, command = lambda: settingsFrame.pack( side="top"))
 settingsButton.pack(side='left')
 
-photo_cam = Image.open('cameraIcon.png')
-photo_cam = photo_cam.resize((240, 200))
+photo_cam = Image.open('root/cameraIcon.png')
+photo_cam = photo_cam.resize((48, 40))
 img_cam = ImageTk.PhotoImage(photo_cam)
-cameraButton = tk.Button(topBar, text="camera", image=img_cam, font=monoFont, command = createButtonFrame)#camera)
+cameraButton = tk.Button(topBar, text="camera", image=img_cam, font=monoFont, command = camera)
 cameraButton.pack(side='right')
 
-#camera()
-createButtonFrame()
+camera()
+#createButtonFrame()
 
 tk.mainloop()
